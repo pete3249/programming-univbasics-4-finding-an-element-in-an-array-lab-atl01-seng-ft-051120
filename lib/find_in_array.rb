6eca344cc0998f4862bd914c164dfd7304f92dc2
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-20 21:12:29 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,12 +1,16 @@
-
-
-
-
-
-
 def find_element_index(array, value_to_find)
- count = 0
+  count = 0
   while count < array.length do
+  puts array[count]
+  counter += 1
+  end
+end
+
+
+
+
+
+
    puts array[count] == value_to_find
    counter += 1
   end 
